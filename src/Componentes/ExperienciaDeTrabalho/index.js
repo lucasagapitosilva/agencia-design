c33b3b9ef10c0848b94b8cf80e6321f059b0f83c
Lucas Agapito <lucasbezerra.rocinha@gmail.com>
2022-12-15 14:59:33 -0300

    atualizacao no componente experiencia do trabalho

--- src/Componentes/ExperienciaDeTrabalho/index.js
+++ src/Componentes/ExperienciaDeTrabalho/index.js
@@ -14,9 +14,23 @@ const ExperienciaDeTrabalho = (props) => {
 
             <div className='container-cards limite-area'>
                 <Card
-                    mes='JUNHO 2012 - 2016' titulo='Web Designer' subtitulo='Pied Piper StartUp.' informacao='Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores' />
-                <Card mes='AGOSTO 2016 - 2019' titulo='Product Designer' subtitulo='E Corp.' informacao='Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra' />
-                <Card mes='FEVEREIRO 2019 - 2021' titulo='Digital Consulting' subtitulo='Arasaka Inc.' informacao='Consultoria em estratégias digitais para todas as etapas do ciclo do projeto, desde a definição inicial até a execução' />
+                    corPadrao={props.corPadrao}
+                    mes='JUNHO 2012 - 2016'
+                    titulo='Web Designer'
+                    subtitulo='Pied Piper StartUp.' informacao='Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores' />
+
+                <Card
+                    corPadrao={props.corPadrao}
+                    mes='AGOSTO 2016 - 2019'
+                    titulo='Product Designer'
+                    subtitulo='E Corp.'
+                    informacao='Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra' />
+
+                <Card
+                    corPadrao={props.corPadrao}
+                    mes='FEVEREIRO 2019 - 2021'
+                    titulo='Digital Consulting'
+                    subtitulo='Arasaka Inc.' informacao='Consultoria em estratégias digitais para todas as etapas do ciclo do projeto, desde a definição inicial até a execução' />
             </div>
         </section>
     )
